adfcb186eb55dc958036ac73fcd7cc56f64fa45e
Timi Salminen <timi_salminen@hotmail.com>
2020-04-10 16:56:41 +0300

    kurssitiedot
    1.1 ja 1.2

---
+++ osa1/kurssitiedot/src/index.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  const Header = () => {
+    return (
+      <>
+        <h1>{course}</h1>
+      </>
+    )
+  }
+
+  const Content = () => {
+    return (
+      <>
+        <p>{part1} {exercises1}</p>
+        <p>{part2} {exercises2}</p>
+        <p>{part3} {exercises3}</p>
+      </>
+    )
+  }
+
+  const Total = () => {
+    return (
+      <>
+        <p>Number of exercises {exercises1 + exercises2 +exercises3}</p>
+      </>
+    )
+  }
+
+  return (
+    <div>
+      <Header />
+      <Content />
+      <Total />
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
